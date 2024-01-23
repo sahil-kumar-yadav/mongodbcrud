cfd3e91719043c4e5a6284ed225ce90dfc5e39b1
SAHIL YADAV <121011155+yadavsk098@users.noreply.github.com>
2024-01-23 22:56:51 +0530

    Final push

--- app/addTopic/page.jsx
+++ app/addTopic/page.jsx
@@ -29,7 +29,7 @@ function AddTopic() {
 
         // code to add to our database
         try {
-            const res = await fetch("http://localhost:3000/api/topics", {
+            const res = await fetch("https://mongodbcrud-liart.vercel.app/api/topics", {
                 method: "POST",
                 headers: {
                     "Content-type": "application/json",
@@ -54,7 +54,7 @@ function AddTopic() {
                 <input onChange={(e) => setTitle(e.target.value)} value={title} className="border border-slate-500 px-8 py-2" type="text" placeholder="Topic Title" />
                 <input onChange={(e) => setDescription(e.target.value)} value={description} className="border border-slate-500 px-8 py-2" type="text" placeholder="Topic Discription" />
 
-                <button onClick={handelSubmit} className="bg-green-600 font-bold text-white py-3 px-6 w-fit">
+                <button type="submit" className="bg-green-600 font-bold text-white py-3 px-6 w-fit">
                     Add topic
                 </button>
             </form>
