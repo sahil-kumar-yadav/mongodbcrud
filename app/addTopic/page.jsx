27f881a640fc3f56bf1db0361c1a8da32eae4b0e
SAHIL YADAV <121011155+yadavsk098@users.noreply.github.com>
2024-01-23 22:54:23 +0530

    minor chanage

--- app/addTopic/page.jsx
+++ app/addTopic/page.jsx
@@ -54,7 +54,7 @@ function AddTopic() {
                 <input onChange={(e) => setTitle(e.target.value)} value={title} className="border border-slate-500 px-8 py-2" type="text" placeholder="Topic Title" />
                 <input onChange={(e) => setDescription(e.target.value)} value={description} className="border border-slate-500 px-8 py-2" type="text" placeholder="Topic Discription" />
 
-                <button onClick={handelSubmit} type="submit" className="bg-green-600 font-bold text-white py-3 px-6 w-fit">
+                <button onClick={handelSubmit} className="bg-green-600 font-bold text-white py-3 px-6 w-fit">
                     Add topic
                 </button>
             </form>
