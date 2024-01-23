3c87ede66759b2910b577fef225527682702b4b4
SAHIL YADAV <121011155+yadavsk098@users.noreply.github.com>
2024-01-23 22:32:07 +0530

    minor changes

--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -7,34 +7,40 @@ import { HiPencilAlt } from "react-icons/hi"
 const getTopics = async () => {
     try {
         // by defult next js store cache so if we update the data we won't get updated data
-        const res = await fetch('http://localhost:3000/api/topics', { cache: "no-store" });
 
+        const res = await fetch("http://localhost:3000/api/topics", {
+            cache: "no-store",
+        });
         // check if its working or not
+
         if (!res.ok) {
             throw new Error("Failed to fetch topics");
         }
+
         return res.json();
     } catch (error) {
-        console.log("Error loading topics ", error);
+        console.log("Error loading topics: ", error);
     }
-}
+};
 
-async function TopicsList() {
 
+export default async function TopicsList() {
     const { topics } = await getTopics();
- 
+
     return (
         <>
-            {topics.map(t => (
-                <div className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start">
+            {topics.map((t) => (
+                <div
+                    key={t._id}
+                    className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start"
+                >
                     <div>
                         <h2 className="font-bold text-2xl">{t.title}</h2>
                         <div>{t.description}</div>
                     </div>
+
                     <div className="flex gap-2">
-                        {/* we need to pass id to delete  */}
                         <RemoveBtn id={t._id} />
-                        {/* for id we have _id  */}
                         <Link href={`/editTopic/${t._id}`}>
                             <HiPencilAlt size={24} />
                         </Link>
@@ -45,4 +51,3 @@ async function TopicsList() {
     );
 }
 
-export default TopicsList;
